6cb1682c4be5ce37a575d57aa22beb74196ab447
louis <louis_laurent@aol.fr>
2018-02-26 16:24:20 +0100

    Des oublis ont été fait xd

--- php/ConnectDB.php
+++ php/ConnectDB.php
@@ -1,22 +1,22 @@
 <?php
 	class ConnectDB{
-		private $hostName;
-		private $dbName;
-		private $charset;
-		private $userName;
-		private $pwd;
+		private $_hostName;
+		private $_dbName;
+		private $_charset;
+		private $_userName;
+		private $_pwd;
 
 		public function __construct($hostNameE, $dbNameE, $charsetE, $userNameE, $pwdE){
 			if(is_string($hostNameE))
-				this->hostName = $hostNameE;
+				this->_hostName = $hostNameE;
 			if(is_string($dbNameE))
-				this->dbName = $dbNameE;
+				this->_dbName = $dbNameE;
 			if(is_string($charsetE))
-				this->charset = $charsetE;
+				this->_charset = $charsetE;
 			if(is_string($userNameE))
-				this->userName = $userNameE;
+				this->_userName = $userNameE;
 			if(is_string($pwdE))
-				this->pwd = $pwdE;
+				this->_pwd = $pwdE;
 			else{
 				print("Erreur Une des données n'est pas une chaine de caractère ! ");
 			}
@@ -24,50 +24,50 @@
 
 		// Getter
 		public function getHostName(){
-			return this->hostName;
+			return this->_hostName;
 		}
 
 		public function getDBName(){
-			return this->dbName;
+			return this->_dbName;
 		}
 
 		public function getCharset(){
-			return this->charset;
+			return this->_charset;
 		}
 
 		public function getUserName(){
-			return this->userName;
+			return this->_userName;
 		}
 
 		public function getPwd(){
-			return this->pwd;
+			return this->_pwd;
 		}
 
 		// Setter
 		public function setHostName($hostNameE){
-			this->hostName = $hostNameE;
+			this->_hostName = $hostNameE;
 		}
 
 		public function setDBName($dbNameE){
-			this->dbName = $dbNameE;
+			this->_dbName = $dbNameE;
 		}
 
 		public function setCharset($charsetE){
-			this->charset = $charsetE;
+			this->_charset = $charsetE;
 		}
 
 		public function setUserName($userNameE){
-			this->userName = $userNameE;
+			this->_userName = $userNameE;
 		}
 
 		public function setPwd($pwdE){
-			this->pwd = $pwdE;
+			this->_pwd = $pwdE;
 		}
 
 		// Les méthodes
 		public function connectToDB(){
 			try{
-				$db = new PDO('mysql:host='.this->hostName.';dbname='.this->dbname.';charset='.this->charset.'', this->userName, this->pwd);
+				$db = new PDO('mysql:host='.this->_hostName.';dbname='.this->_dbname.';charset='.this->_charset.'', this->_userName, this->_pwd);
 
 			}catch(Exception $e){
 				die('Erreur : ' . e->getMessage());
@@ -78,7 +78,7 @@
 		// Retour affichage
 
 		public function to_string(){
-			return "Le host : ".this->hostName." le nom de la base de données : ".this->dbName." le type d'encodage : ".this->charset." le nom de l'utilisateur : ".this->userName." le mot de passe : ".this->pwd;
+			return "Le host : ".this->_hostName." le nom de la base de données : ".this->_dbName." le type d'encodage : ".this->_charset." le nom de l'utilisateur : ".this->_userName." le mot de passe : ".this->_pwd;
 		}
 
 
